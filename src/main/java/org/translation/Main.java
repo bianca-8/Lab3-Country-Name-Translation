a0f523eb4a1593837c0cbdab47a028b4dde8e492
Ahmad <ahmad@Ahmads-MacBook-Pro.local>
2026-05-21 19:51:30 -0400

    hello

--- src/main/java/org/translation/Main.java
+++ src/main/java/org/translation/Main.java
@@ -73,16 +73,15 @@ public class Main {
         List<String> codes = translator.getCountries();
         List<String> names = new ArrayList<>();
 
-        for (int i = 0; i < codes.size(); i++) {
-            String code = codes.get(i);
+        for (String code : codes) {
             String name = converter.fromCountryCode(code);
             names.add(name);
         }
 
         Collections.sort(names);
 
-        for (int i = 0; i < names.size(); i++) {
-            System.out.println(names.get(i));
+        for (String name : names) {
+            System.out.println(name);
         }
         System.out.println("select a country from above:");
 
