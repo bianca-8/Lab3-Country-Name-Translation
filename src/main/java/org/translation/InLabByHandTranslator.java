6848097e51e96c464ba2e8270dcac3085966515f
Bianca <130195861+bianca-8@users.noreply.github.com>
2026-05-21 18:50:53 -0400

    fix checkstyle

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -14,6 +14,9 @@ import java.util.List;
  * the country code "can" to several languages.
  */
 public class InLabByHandTranslator implements Translator {
+
+    public static final String CANADA = "can";
+
     /**
      * Returns the language abbreviations for all languages whose translations are
      * available for the given country.
@@ -24,14 +27,11 @@ public class InLabByHandTranslator implements Translator {
     @Override
     public List<String> getCountryLanguages(String country) {
         // TODO Checkstyle: The String "can" appears 4 times in the file.
-        if ("can".equals(country)) {
+        if (CANADA.equals(country)) {
             return new ArrayList<>(List.of("de", "en", "zh"));
         }
         return new ArrayList<>();
     }
-
-    // TODO Checkstyle: Static variable definition in wrong order.
-    public static final String CANADA = "can";
 
     /**
      * Returns the country abbreviations for all countries whose translations are
@@ -41,7 +41,7 @@ public class InLabByHandTranslator implements Translator {
      */
     @Override
     public List<String> getCountries() {
-        return new ArrayList<>(List.of("can"));
+        return new ArrayList<>(List.of(CANADA));
     }
 
     /**
@@ -53,21 +53,19 @@ public class InLabByHandTranslator implements Translator {
      */
     @Override
     public String translate(String country, String language) {
-        // TODO Checkstyle: Return count is 5 (max allowed for non-void methods/ lambdas is 2).
-        if (!"can".equals(country)) {
+        String word = null;
+        if (!CANADA.equals(country)) {
             return null;
         }
         if ("de".equals(language)) {
-            return "Kanada";
+            word = "Kanada";
         }
         else if ("en".equals(language)) {
-            return "Canada";
+            word = "Canada";
         }
         else if ("zh".equals(language)) {
-            return "加拿大";
+            word = "加拿大";
         }
-        else {
-            return null;
-        }
+        return word;
     }
 }
